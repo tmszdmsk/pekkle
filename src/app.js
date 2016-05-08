3138cce61266b6b83106da67b394be0894e2ce3d
Tomasz Adamski <tmszdmsk@users.noreply.github.com>
2016-05-08 21:55:24 +0200

    First schedule displayed on screen

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 var busStops = require('bus_stops');
 var Q = require('q');
 var location = require('location');
-var UI = require('UI');
+var UI = require('ui');
 
 var splashScreen = new UI.Card({
   "title": "Loading",
   "body": "wait patiently"
 });
 splashScreen.show();
-Q.all([location.location(), busStops.busStops()])
+var closestStop = Q.all([location.location(), busStops.busStops()])
 .spread(busStops.sortByDistance)
-.then(function(stops){
+.then(function(allStops){
+  return allStops[0];
+});
+var scheduleInfo = closestStop.then(function(stop){return stop.przystanek.id;}).then(busStops.scheduleInfo);
+
+Q.all([closestStop, scheduleInfo])
+.spread(function(closestStop, scheduleInfo){
   console.log('stops returned');
-  var closestBusStopInfo = UI.Card({
-    "title": stops[0].przystanek.properties.stop_name,
-    "subtitle": "Distance: "+stops[0].distance+"m"
+  console.log(JSON.stringify(closestStop));
+  console.log(JSON.stringify(scheduleInfo));
+  var closestBusStopInfo = new UI.Card({
+    "title": closestStop.przystanek.properties.stop_name,
+    "subtitle": closestStop.distance+"m",
+    "body": scheduleInfo.success.times.map(function(singleTime){
+      return singleTime.line+"->"+singleTime.direction+": "+singleTime.minutes+"min";
+    }).reduce(function(prev, elem){return prev+elem+"\n";}, "")
   });
   closestBusStopInfo.show();
   splashScreen.hide();
-});
+}).done();
--- src/bus_stops.js
+++ src/bus_stops.js
@@ -2,7 +2,7 @@ var Q = require('q');
 if (typeof(Number.prototype.toRadians) === "undefined") {
   Number.prototype.toRadians = function() {
     return this * Math.PI / 180;
-  }
+  };
 }
 
 var measure_distance = function(a,b){
@@ -47,11 +47,35 @@ var closest = function(position, busStops){
   console.log("sorting bus stops by distance");
   return busStops.features.map(function(elem){
     var distance = measure_distance(position.coords, {latitude: elem.geometry.coordinates[1], longitude:elem.geometry.coordinates[0]});
-    return {przystanek: elem, distance: distance}
-  }).sort(function(a,b){return a.distance-b.distance})
+    return {przystanek: elem, distance: Math.round(distance)};
+  }).sort(function(a,b){return a.distance-b.distance;});
+};
+
+var pekaInfo = function(busStopId){
+  var deferred = Q.defer();
+  console.log("pekaInfo in");
+  var method = 'POST';
+  var url = 'https://www.peka.poznan.pl/vm/method.vm';
+  // Create the request
+  var request = new XMLHttpRequest();
+  request.onload = function() {
+    console.log('pekaInfo response');
+    deferred.resolve(JSON.parse(this.responseText));
+  };
+  request.onerror = function() {
+    console.log("errror peka");
+  };
+
+  // Send the request
+  request.open(method, url, true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.send('method=getTimes&p0=%7B%22symbol%22%3A%22'+busStopId+'%22%7D');
+  return deferred.promise;
 }
+
 
 this.exports = {
   "busStops" : loadBusStops,
-  "sortByDistance" : closest
+  "sortByDistance" : closest,
+  "scheduleInfo": pekaInfo
 };
