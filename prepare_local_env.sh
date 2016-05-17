81052a6839bd4a97347459face39942b21889dcc
Tomasz Adamski <tomasz.adamski@payu.pl>
2016-05-17 22:57:41 +0200

    local dev environment script

---
+++ prepare_local_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "call it with source or . if you want to have pebble alias created"
+rm -rf ./dist
+wget -q https://github.com/pebble/pebblejs/archive/master.zip -O /tmp/pebblejs-master.zip
+unzip  -o -q /tmp/pebblejs-master.zip -d ./dist
+cp ./appinfo.json ./dist/pebblejs-master/
+cp -r ./src/* ./dist/pebblejs-master/src/js/
+alias pebble='function _pebble_alias(){ (cd ./dist/pebblejs-master/; pebble $@;) };_pebble_alias'
